5fc990c88587bc4b659eed8d1e564253cbfba7c9
Barroso Montana Santiago <sbarroso@emea.msad.sopra>
2015-11-27 12:15:58 +0100

    Add file used to deal with proxies

---
+++ chapter3/proxies.js
@@ -0,0 +1,27 @@
+var ProxyAgent = require('https-proxy-agent');
+var superagent = require('superagent');
+var superagentProxy = require('superagent-proxy');
+
+superagentProxy(superagent);
+
+module.exports = function() {
+
+	var proxy = process.env.http_proxy || 'http://alca.proxy.corp.sopra:8080';
+
+	var initStripe = function(stripe) {
+		stripe.setHttpAgent(new ProxyAgent(proxy));
+	};
+
+	var getSuperAgent = function(url) {
+		if (proxy) {
+			return superagent.get(url).proxy(proxy);
+		} else {
+			return superagent;
+		}
+	};
+
+	return {
+		initStripe: initStripe,
+		getSuperAgent: getSuperAgent
+	};
+};
